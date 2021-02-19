d50439ee0907b2294f877a34a2b0330b8e373806
student <student@ip-172-31-54-199.us-west-2.compute.internal>
2021-02-18 16:55:18 -0800

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,39 @@
 package _01_houses;
 
+import java.awt.Color;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot nugget = new Robot();
+	
 	public void run() {
+		nugget.moveTo(75, 500);
+		drawHouse();
+		for (int j = 0; j < 9; j++) {
+			drawHouse();
+			
+		}
+		}
+	
+	
+	void drawHouse() {
 		
+		Random ran = new Random();
+		int i = ran.nextInt(126);
+		nugget.penDown();
+		nugget.setRandomPenColor();
+		nugget.setSpeed(10);
+		nugget.hide();
+		nugget.move(i+75);
+		nugget.turn(90);
+		nugget.move(50);
+		nugget.turn(90);
+		nugget.move(i+75);
+		nugget.turn(-90);
+		nugget.setPenColor(Color.GREEN);
+		nugget.move(25);
+		nugget.turn(-90);
 	}
 }
