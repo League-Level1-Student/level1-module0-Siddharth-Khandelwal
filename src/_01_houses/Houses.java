42487fe7fe2259a627bf7f0057d0e3ef8bf38178
root <root@53527ae3a108>
2021-02-24 00:51:56 +0000

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -8,18 +8,39 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	Robot nugget = new Robot();
 	
+		
+	
 	public void run() {
-		nugget.moveTo(75, 500);
+		nugget.setWindowColor(Color.black);
+		nugget.hide();
+		nugget.setSpeed(100000000);
+		nugget.penDown();
+		nugget.setPenColor(Color.GREEN);
+		nugget.setSpeed(10);
+		nugget.moveTo(0, 500);
+		nugget.turn(90);
+		nugget.move(75);
+		nugget.turn(-90);
 		drawHouse();
 		for (int j = 0; j < 9; j++) {
 			drawHouse();
 			
 		}
+		nugget.setPenColor(Color.GREEN);
+		nugget.turn(90);
+		nugget.move(100);
+		int j = 600;
+		nugget.setPenWidth(5);
+		for (int i = 0; i < 20; i++) {
+			j-=5;
+			nugget.setPenColor(Color.GREEN);
+			nugget.moveTo(0, j);
+			nugget.move(1000);
 		}
+	}
 	
 	
 	void drawHouse() {
-		
 		Random ran = new Random();
 		int i = ran.nextInt(126);
 		nugget.penDown();
