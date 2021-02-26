42f2be2d264a75bba283becd2756c8e2a5db3ca5
student <student@ip-172-31-61-79.us-west-2.compute.internal>
2021-02-25 16:52:28 -0800

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -15,7 +15,7 @@ public class Houses {
 		nugget.hide();
 		nugget.setSpeed(100000000);
 		nugget.penDown();
-		nugget.setPenColor(Color.GREEN);
+		nugget.setPenColor(Color.green);
 		nugget.setSpeed(10);
 		nugget.moveTo(0, 500);
 		nugget.turn(90);
@@ -26,20 +26,30 @@ public class Houses {
 			drawHouse();
 			
 		}
-		nugget.setPenColor(Color.GREEN);
+		nugget.setPenColor(Color.green);
 		nugget.turn(90);
 		nugget.move(100);
 		int j = 600;
 		nugget.setPenWidth(5);
 		for (int i = 0; i < 20; i++) {
 			j-=5;
-			nugget.setPenColor(Color.GREEN);
+			nugget.setPenColor(Color.green);
 			nugget.moveTo(0, j);
 			nugget.move(1000);
 		}
 	}
-	
-	
+	void drawPointyRoof() {
+		nugget.turn(45);
+		nugget.move(40);
+		nugget.turn(90);
+		nugget.move(40);
+		nugget.turn(45);
+	}
+	void drawFlatRoof() {
+	nugget.turn(90);
+		nugget.move(50);
+		nugget.turn(90);
+	}
 	void drawHouse() {
 		Random ran = new Random();
 		int i = ran.nextInt(126);
@@ -48,12 +58,15 @@ public class Houses {
 		nugget.setSpeed(10);
 		nugget.hide();
 		nugget.move(i+75);
-		nugget.turn(90);
-		nugget.move(50);
-		nugget.turn(90);
+		if(i+75>140) {
+			drawFlatRoof();
+		}
+		else {
+		drawPointyRoof();	
+		}
 		nugget.move(i+75);
 		nugget.turn(-90);
-		nugget.setPenColor(Color.GREEN);
+		nugget.setPenColor(Color.green);
 		nugget.move(25);
 		nugget.turn(-90);
 	}
