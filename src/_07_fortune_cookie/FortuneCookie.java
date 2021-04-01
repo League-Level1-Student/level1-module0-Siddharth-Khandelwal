9e88d90a9eedae865b92567277682541cf049710
student <student@ip-172-31-58-25.us-west-2.compute.internal>
2021-04-01 16:53:50 -0700

    Fortune Cookie

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,11 +1,36 @@
 package _07_fortune_cookie;
 
-public class FortuneCookie {
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class FortuneCookie implements ActionListener {
 	
         public void showButton() {
-            System.out.println("Button clicked");
-       
+            JFrame frame = new JFrame();
+            JPanel panel = new JPanel();
+            JButton button = new JButton();
+        	System.out.println("Button clicked");
+       frame.setVisible(true);
+       frame.add(button);
+       frame.pack();
+       button.addActionListener(this);
+       Random rand = new Random();
+    		   rand.nextInt(5);
         }
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			JOptionPane.showMessageDialog(null, "WooHoo");
+		}
     }
       
    
