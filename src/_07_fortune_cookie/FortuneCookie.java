c7b01f8f8148ad312f89a5247e9a4955f8212125
student <student@ip-172-31-48-59.us-west-2.compute.internal>
2021-04-06 16:53:37 -0700

    FortuneCookie

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,7 +1,5 @@
 package _07_fortune_cookie;
 
-
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -12,26 +10,36 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class FortuneCookie implements ActionListener {
-	
-        public void showButton() {
-            JFrame frame = new JFrame();
-            JPanel panel = new JPanel();
-            JButton button = new JButton();
-        	System.out.println("Button clicked");
-       frame.setVisible(true);
-       frame.add(button);
-       frame.pack();
-       button.addActionListener(this);
-       Random rand = new Random();
-    		   rand.nextInt(5);
-        }
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			JOptionPane.showMessageDialog(null, "WooHoo");
+	public void showButton() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton button = new JButton();
+		System.out.println("Button clicked");
+		frame.setVisible(true);
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+		Random rand = new Random();
+		int randomnumber = rand.nextInt(5);
+		if (randomnumber == 0) {
+			JOptionPane.showMessageDialog(null, "You will eat an apple!");
+		} else if (randomnumber == 1) {
+			JOptionPane.showMessageDialog(null, "You will eat a cake!");
+		} else if (randomnumber == 2) {
+			JOptionPane.showMessageDialog(null, "You will eat an orange!");
+		} else if (randomnumber == 3) {
+			JOptionPane.showMessageDialog(null, "You will eat a bowl of ice cream!");
 		}
-    }
-      
-   
-
+		else if (randomnumber == 4) {
+			JOptionPane.showMessageDialog(null, "You will eat a bowl of grapes!");
+		}
+	}
+}
