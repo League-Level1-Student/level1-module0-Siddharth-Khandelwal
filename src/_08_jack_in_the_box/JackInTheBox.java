b62ca90ac1717d3959395fb1d459a651449a3d22
student <student@ip-172-31-49-220.us-west-2.compute.internal>
2021-04-20 16:55:22 -0700

    Jack In The Box

--- src/_08_jack_in_the_box/JackInTheBox.java
+++ src/_08_jack_in_the_box/JackInTheBox.java
@@ -2,15 +2,20 @@ package _08_jack_in_the_box;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.Random;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class JackInTheBox implements ActionListener {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
-JButton button = new JButton();
+JButton button = new JButton("Click for a possible suprise!");
 JackInTheBox(){
 System.out.println("Button clicked");
 frame.setVisible(true);
@@ -18,10 +23,47 @@ frame.add(button);
 frame.pack();
 button.addActionListener(this);
 }
+Random rand = new Random();
+int counter =rand.nextInt(6)+5;
+int bob = 0;		
 @Override
+
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
 
+	bob+=1;
+	if (bob == counter){
+	showPicture("jackInTheBox.png");	
+
+}
+}
+private void showPicture(String fileName) { 
+    try {
+         JLabel imageLabel = createLabelImage(fileName);
+         JFrame frame = new JFrame();
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.add(imageLabel);
+         frame.setVisible(true);
+         frame.pack();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+private JLabel createLabelImage(String fileName) {
+    try {
+         URL imageURL = getClass().getResource(fileName);
+         if (imageURL == null) {
+              System.err.println("Could not find image " + fileName);
+              return new JLabel();
+         } else {
+              Icon icon = new ImageIcon(imageURL);
+              JLabel imageLabel = new JLabel(icon);
+              return imageLabel;
+         }
+    } catch (Exception e) {
+         System.err.println("Could not find image " + fileName);
+         return new JLabel();
+    }
 }
 
 }
