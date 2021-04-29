9f0e8bacd9799b904d388589bedf1d9973f8d4d4
student <student@ip-172-31-54-131.us-west-2.compute.internal>
2021-04-29 16:54:27 -0700

    CutenessTv

--- src/_10_cuteness_tv/CutenessTV.java
+++ src/_10_cuteness_tv/CutenessTV.java
@@ -10,33 +10,39 @@ import javax.swing.JPanel;
 
 public class CutenessTV implements ActionListener {
 	JButton button = new JButton("Panda Video");
-		JButton button1 = new JButton("Tiny Animals");
-		public void showButton() {
+	JButton button1 = new JButton("Tiny Animals");
+	JButton button2 = new JButton("Koala");
+
+	public void showButton() {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
-		
+
 		System.out.println("Button clicked");
 		frame.setVisible(true);
 		frame.add(panel);
 		panel.add(button);
 		panel.add(button1);
+		panel.add(button2);
 		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		button.addActionListener(this);
-button1.addActionListener(this);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		if(event.getSource()==button) {
-			
-		JOptionPane.showMessageDialog(null, "PANDA, Panda Panda Panda Panda");
+		if (event.getSource() == button) {
+
+			JOptionPane.showMessageDialog(null, "PANDA, Panda Panda Panda Panda");
 		}
-		else if{
+
+		else if (event.getSource() == button1) {
 			JOptionPane.showMessageDialog(null, "Baby Animals");
+		} else {
+			JOptionPane.showMessageDialog(null, "Koala Gang");
 		}
-		else {
-			JOptionPane.showMessageDialog(null, "");
-		}
-}
+
+	}
 }
